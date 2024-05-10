943b215845c7469a175625b098a03bd7053f7939
Raphael Soares <raphaelrdsoares@gmail.com>
2024-05-10 16:58:08 -0300

    feat: creates project with Angular

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-root",
+    templateUrl: "./app.html",
+    styleUrls: [],
+})
+export class AppComponent {
+    title = "casamento-raquel-igor-frontend-web-site";
+}
