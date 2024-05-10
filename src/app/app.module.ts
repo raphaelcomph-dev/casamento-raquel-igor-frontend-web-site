c63fcc05cbf3f9411a0e3bc6250836f68dd21d22
Raphael Soares <raphaelrdsoares@gmail.com>
2024-05-10 18:42:28 -0300

    feat: import habibi theme and creates hero component #WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,30 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app";
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { MainHeroComponent } from "./views/components/organisms/main-hero/main-hero.component";
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from "ngx-mask";
+import { NgxSpinnerModule } from "ngx-spinner";
+import { ToastrModule } from "ngx-toastr";
+import { MainPage } from "./views/pages/main/main.page";
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, NgbModule],
-    providers: [],
+    declarations: [AppComponent, MainHeroComponent, MainPage],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgbModule,
+        NgxMaskDirective,
+        NgxMaskPipe,
+        NgxSpinnerModule,
+        ToastrModule.forRoot({
+            timeOut: 5000,
+            positionClass: "toast-top-center",
+            preventDuplicates: true,
+            progressBar: true,
+        }),
+    ],
+    providers: [provideNgxMask()],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
