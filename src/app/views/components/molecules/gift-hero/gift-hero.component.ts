5c0aaa1a8e70d3e6e17b95919dc3b4f1edada62d
Raphael Soares <raphaelrdsoares@gmail.com>
2024-06-18 23:15:34 -0300

    feat: includes subtitle in gift hero component

--- src/app/views/components/molecules/gift-hero/gift-hero.component.ts
+++ src/app/views/components/molecules/gift-hero/gift-hero.component.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: "app-gift-hero",
     templateUrl: "./gift-hero.component.html",
     styleUrls: ["./gift-hero.component.scss"],
 })
-export class GiftHeroComponent {}
+export class GiftHeroComponent {
+    @Input("subtitle") subtitle: string;
+}
