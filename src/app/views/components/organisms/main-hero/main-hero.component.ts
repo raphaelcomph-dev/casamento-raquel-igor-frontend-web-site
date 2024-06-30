011045fdb8677f18044e472702cf9aaf32d2c6a3
Raphael Soares <raphaelrdsoares@gmail.com>
2024-06-30 20:00:09 -0300

    style: gift list link in main hero

--- src/app/views/components/organisms/main-hero/main-hero.component.ts
+++ src/app/views/components/organisms/main-hero/main-hero.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import * as dayjs from "dayjs";
 import { Duration } from "dayjs/plugin/duration";
+import { BaseView } from "../../../base.view";
 
 @Component({
     selector: "app-main-hero",
     templateUrl: "./main-hero.component.html",
 })
-export class MainHeroComponent implements OnInit {
+export class MainHeroComponent extends BaseView implements OnInit {
     private readonly ONE_SECOND_IN_MILISEC = 1000;
 
     targetDate = dayjs("2024-10-24 21:00:00");
